584bce66aa77deec2cdb6bc7726dd3345e3df5b4
Yungsaas <45827473+Yungsaas@users.noreply.github.com>
2025-09-08 18:44:16 +0200

    Revert "added code functionality for saving replay data temporarily"
    This reverts commit 6e8fdc7925c10bbb78d9b03fb9017123dd8a9518.

--- src/temp_save_replay.cpp
+++ src/temp_save_replay.cpp
@@ -5,7 +5,6 @@
 #include "godot_cpp/core/class_db.hpp"
 #include "godot_cpp/core/print_string.hpp"
 #include "godot_cpp/variant/string.hpp"
-#include <tuple>
 
 bool Temp_save_replay::add_node(godot::Node* node)
 {
@@ -22,36 +21,8 @@ void Temp_save_replay::debug_print_array()
     }
 }
 
-void Temp_save_replay::start_recording()
-{
-    temporary_saved_data.clear();
-    recordingFrame = 0;
-    recording = true;
-}
-
-void Temp_save_replay::stop_recording()
-{
-    recordingFrame = 0;
-    recording = false;
-}
-
-void Temp_save_replay::update()
-{
-    if(!recording) return;
-
-    for (auto nodeIn : tracked_nodes)
-    {
-        temporary_saved_data.emplace(std::make_tuple(nodeIn,recordingFrame), nodeIn.TRANSFORM3D);
-    }
-
-    recordingFrame++;
-}
-
 void Temp_save_replay::_bind_methods()
 {
     godot::ClassDB::bind_method(godot::D_METHOD("debug_print_array"), &Temp_save_replay::debug_print_array);
     godot::ClassDB::bind_method(godot::D_METHOD("add_node", "node"), &Temp_save_replay::add_node);
-    godot::ClassDB::bind_method(godot::D_METHOD("start_recording"), &Temp_save_replay::start_recording);
-    godot::ClassDB::bind_method(godot::D_METHOD("stop_recording"), &Temp_save_replay::stop_recording);
-    godot::ClassDB::bind_method(godot::D_METHOD("update"), &Temp_save_replay::update);
 }
--- src/temp_save_replay.hpp
+++ src/temp_save_replay.hpp
@@ -2,17 +2,7 @@
 
 #include "godot_cpp/classes/node.hpp"
 #include "godot_cpp/classes/wrapped.hpp"
-#include "godot_cpp/variant/array.hpp"
 #include "godot_cpp/variant/string.hpp"
-#include "godot_cpp/variant/vector3.hpp"
-
-struct nodePosTrack
-{
-    int frame;
-    float posX;
-    float posY;
-    float posZ;
-};
 
 class Temp_save_replay:public godot::Node{
 
@@ -25,18 +15,10 @@ class Temp_save_replay:public godot::Node{
 
     private:
     godot::Array tracked_nodes;
-    std::unordered_map<std::tuple<godot::Node*, int>, godot::Vector3> temporary_saved_data;
-    bool recording = false;
-    int recordingFrame = 0;
     
     public:
     bool add_node(godot::Node* node);
 
     void debug_print_array();
-
-    void start_recording();
-    void stop_recording();
-
-    void update();
-
+    
 };
