fdcffe1c279a29557bde076be4e86e92d6d5b3f2
Yungsaas <45827473+Yungsaas@users.noreply.github.com>
2025-09-08 15:11:18 +0200

    adding needed classes

---
+++ src/button_manager.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "godot_cpp/classes/base_button.hpp"
+#include "godot_cpp/classes/button.hpp"
+#include "godot_cpp/classes/node.hpp"
+#include "godot_cpp/variant/string.hpp"
+#include "temp_save_replay.hpp"
+
+class MenuButton:public godot::Button{
+};
