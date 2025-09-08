a9a93f48c1ca65b563fcd04edcac3646a94d37c0
Harry Ashikian <harry.ashikian@gmail.com>
2025-09-08 20:19:05 +0200

    added a selection pannel

--- physics_platformer/addons/replay_qol/replay_qol.gd
+++ physics_platformer/addons/replay_qol/replay_qol.gd
@@ -1,18 +1,25 @@
 @tool
 extends EditorPlugin
 
-var panel
+var record_panel
+const RECORD_PANEL = preload("res://addons/replay_qol/Record_Panel.tscn")
 
-const TOOL_PANEL = preload("res://addons/replay_qol/Record_Panel.tscn")
+var selection_panel
+const SELECTION_PANEL = preload("res://addons/replay_qol/Selection_Panel.tscn")
 
 func _enter_tree() -> void:
-	panel = TOOL_PANEL.instantiate()
-	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_BL, panel)
+	record_panel = RECORD_PANEL.instantiate()
+	add_control_to_bottom_panel(record_panel, "Record_Panel")
+	
+	selection_panel = SELECTION_PANEL.instantiate()
+	add_control_to_dock(DOCK_SLOT_RIGHT_UL, selection_panel)
 	
 
 
 func _exit_tree() -> void:
-	remove_control_from_docks(panel)
+	remove_control_from_bottom_panel(record_panel)
+	record_panel.queue_free()
 	
-	panel.queue_free()
+	remove_control_from_bottom_panel(selection_panel)
+	selection_panel.queue_free()
 	
