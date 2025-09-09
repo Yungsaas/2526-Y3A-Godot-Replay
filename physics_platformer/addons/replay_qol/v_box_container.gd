3d2392aa63593268311ecc01def0d66fdb8495d0
Harry Ashikian <harry.ashikian@gmail.com>
2025-09-09 15:23:07 +0200

    added remove node button to the selection panel

--- physics_platformer/addons/replay_qol/v_box_container.gd
+++ physics_platformer/addons/replay_qol/v_box_container.gd
@@ -1,17 +1,18 @@
 @tool
 extends Control
 
-var editor_interface  # Remove the type hint
+var editor_interface
 
 
-@onready var add_button: Button = $VBoxContainer/Button
+@onready var add_button: Button = $VBoxContainer/AddButton
+@onready var removeButton: Button = $VBoxContainer/Button2
 @onready var tree: Tree = $VBoxContainer/Tree
 var nodes: Array[NodePath] = []
 
 func _ready():
 	tree.create_item()
 
-func _on_button_pressed() -> void:
+func _on_add_button_pressed() -> void:
 	if editor_interface == null:
 		push_error("EditorInterface is null! Make sure plugin passes it correctly.")
 		return
@@ -24,6 +25,8 @@ func _on_button_pressed() -> void:
 			nodes.append(path)
 			_add_to_tree(node)
 			print("Node added in list")
+			
+			
 		else:
 			print("Node already in list")
 
@@ -33,3 +36,23 @@ func _add_to_tree(node: Node):
 	var item = tree.create_item(root_item)
 	item.set_text(0, node.name)
 	item.set_metadata(0, node.get_path())
+	
+
+func _on_button_2_pressed() -> void:
+	var selected_item = tree.get_selected()
+	if selected_item:
+		var path = selected_item.get_metadata(0)
+		if path in nodes:
+			nodes.erase(path)  # Remove from your array
+			print("Node removed from list")
+		else:
+			print("Node not in list")
+		
+		# Remove from tree
+		var parent = selected_item.get_parent()
+		if parent:
+			parent.remove_child(selected_item)
+		else:
+			tree.set_root(null)  # In case it’s the root
+	else:
+		print("No item selected")
