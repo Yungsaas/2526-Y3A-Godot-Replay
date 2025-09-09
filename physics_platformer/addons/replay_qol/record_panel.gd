e2d0cdd0e1f0a194ffc629b4baf2fe052592b948
Yungsaas <45827473+Yungsaas@users.noreply.github.com>
2025-09-09 14:23:11 +0200

    Added gs script portion of recording button functionality

--- physics_platformer/addons/replay_qol/record_panel.gd
+++ physics_platformer/addons/replay_qol/record_panel.gd
@@ -1,7 +1,22 @@
 @tool
 extends Control
 
+var recorder: Temp_save_replay
+var is_recording: bool = false
 
+func set_recorder(new_recorder: Temp_save_replay) -> void:
+	recorder = new_recorder
 
 func _on_button_pressed() -> void:
+	if recorder:
+		if is_recording:
+			recorder.stop_recording()
+			print("Stop recording.")
+			recorder.debug_print_positions()
+			is_recording = false
+		else:
+			recorder.start_recording()
+			print("Start recording")
+			recorder.debug_print_array()
+			is_recording = true
 	pass # Replace with function body.
