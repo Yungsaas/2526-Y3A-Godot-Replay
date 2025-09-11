1624cd1a22070bdbca398f99e4e64215dcb9b286
Yungsaas <45827473+Yungsaas@users.noreply.github.com>
2025-09-11 17:04:50 +0200

    added framework for replay

--- physics_platformer/recorder.gd
+++ physics_platformer/recorder.gd
@@ -1,9 +1,10 @@
 extends Temp_save_replay
 
 var is_recording: bool = false
+var is_replaying: bool = false
 
 func _ready() -> void:
-	#add_node($"../Player")
+	add_node($"../Player")
 	pass
 
 func _physics_process(_delta: float) -> void:
@@ -11,6 +12,10 @@ func _physics_process(_delta: float) -> void:
 	#get input
 	var start_recording_input := Input.is_action_pressed(&"start_recording")
 	var stop_recording_input := Input.is_action_pressed(&"stop_recording")
+	
+	
+	var start_replay_input := Input.is_action_pressed(&"start_replay")
+	var stop_replay_input := Input.is_action_pressed(&"stop_replay")
 	
 	if start_recording_input and not is_recording:
 		print("Start Recording")
@@ -21,5 +26,12 @@ func _physics_process(_delta: float) -> void:
 		print("Stop Recording")
 		stop_recording()
 		is_recording = false
+		
+	if start_replay_input:
+		is_replaying = true
+		
+	if stop_replay_input:
+		is_replaying = false
+		
 	
 	update()
