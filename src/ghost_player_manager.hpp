9bc3c31bce97fa02acbcda8ffb3eb57058656e75
Yungsaas <45827473+Yungsaas@users.noreply.github.com>
2025-10-27 11:23:13 +0100

    Added recording feature for ghost player
    Added:
    recording feature
    access to all relevant functions in gdscript
    loading replay from memory

--- src/ghost_player_manager.hpp
+++ src/ghost_player_manager.hpp
@@ -1,6 +1,8 @@
 #pragma once
+#include "godot_cpp/classes/json.hpp"
 #include "godot_cpp/classes/node.hpp"
 #include "godot_cpp/classes/packed_scene.hpp"
+#include "godot_cpp/classes/ref.hpp"
 #include "godot_cpp/variant/node_path.hpp"
 
 class Ghost_Player_Manager : public godot::Node
@@ -22,35 +24,54 @@ godot::PackedScene player_packed_scene;
 godot::String player_packed_scene_path;
 
 //Temporary data maps for ghost player replay
-std::unordered_map<int, godot::Vector3> temporary_data_map_3d_pos; //Recorded data for 3D positions
-std::unordered_map<int, godot::Vector2> temporary_data_map_2d_pos; //Recorded data for 2D positions
+std::unordered_map<int, godot::Vector3> temporary_data_map_3d_rep;
+std::unordered_map<int, godot::Vector2> temporary_data_map_2d_rep;
+
+//Temporary data maps for ghost player recording
+std::unordered_map<int, godot::Vector3> temporary_data_map_3d_rec;
+std::unordered_map<int, godot::Vector2> temporary_data_map_2d_rec;
 
 void load_json_file();
 
 bool ghost_replaying = false;
+bool ghost_recording = false;
 int ghost_replay_current_frame = 0;
 int ghost_replay_max_frame;
+int ghost_recording_frame = 0;
 
 godot::Node* ghost_player_node;
 godot::NodePath ghost_player_node_path;
+
+godot::String json_replay_path;
+godot::Ref<godot::JSON> json_replay_path_ref;
+
 
 void disable_physics_for_ghost();
 void remove_script_from_ghost();
 void delete_ghost_player();
 void replay_position();
+void record_ghost();
 
 public:
 
 void set_player_node(godot::Node* new_player_node);
 godot::Node* get_player_node();
 
-void set_replay_path();
+void set_replay_path(godot::String new_path);
 godot::String get_replay_path();
 
 void start_ghost_replay();
 void pause_ghost_replay();
 void continue_ghost_replay();
 void stop_ghost_replay();
+
+void start_ghost_recording();
+void stop_ghost_recording();
+
+void load_ghost_recording_from_memory();
+
+void save_ghost_recording_to_json(); //TODO
+void load_ghost_recording_from_json(); //TODO
 
 void update();
 };
