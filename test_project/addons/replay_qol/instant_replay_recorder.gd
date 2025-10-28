5259d8c4180f06b5e665866c4d693f37d4d6c1e6
Yungsaas <45827473+Yungsaas@users.noreply.github.com>
2025-10-28 13:27:05 +0100

    added plug in tscns into test project

---
+++ test_project/addons/replay_qol/instant_replay_recorder.gd
@@ -0,0 +1,54 @@
+extends InstantReplayRecorder
+
+# CTRL+K for start recording SHIFT+K for start replay
+# CTRL+L for stop recording SHIFT+K for stop replay
+
+var is_recording: bool = true
+var is_replaying: bool = false
+
+@export var nodes_array: Array[Node] = []
+@export var json_path: JSON
+@export var json_3d_path: JSON
+@export var input_json_path: JSON
+@export var custom_json_path: JSON
+
+func _ready() -> void:
+	set_tracked_nodes(nodes_array)
+	 
+	set_json_path(json_path)
+	set_3d_json_path(json_3d_path)
+	set_input_json_path(input_json_path)
+	set_custom_json_path(custom_json_path)
+	
+	load_json_file()
+	start_recording()
+	pass
+
+func _on_btn_pressed() -> void:
+	print(nodes_array[0])
+	pass
+
+func _physics_process(_delta: float) -> void:
+	
+	#get input
+	var start_recording_input := Input.is_action_just_pressed(&"start_recording")
+	var stop_recording_input := Input.is_action_just_pressed(&"stop_recording")
+	
+	
+	var start_replay_input := Input.is_action_just_pressed(&"start_replay")
+	var stop_replay_input := Input.is_action_just_pressed(&"stop_replay")
+	
+
+	if start_replay_input:
+		stop_recording()
+		print("Start replaying")
+		start_replay()
+
+	if stop_replay_input:
+		print("Stop replaying manually")
+		stop_replay()
+		start_recording()
+	
+	#check_input()
+	
+	update()
---
+++ test_project/addons/replay_qol/recorder.gd
@@ -0,0 +1,62 @@
+extends Recorder
+
+# CTRL+K for start recording SHIFT+K for start replay
+# CTRL+L for stop recording SHIFT+K for stop replay
+
+var is_recording: bool = false
+var is_replaying: bool = false
+
+@export var nodes_array: Array[Node] = []
+@export var json_path: JSON
+@export var input_json_path: JSON
+@export var custom_json_path: JSON
+
+func _ready() -> void:
+	set_tracked_nodes(nodes_array)
+	
+	set_json_path(json_path)
+	set_input_json_path(input_json_path)
+	set_custom_json_path(custom_json_path)
+	
+	load_json_file()
+	
+	add_custom_data($"../Player", "coin_counter")
+	
+	pass
+
+func _on_btn_pressed() -> void:
+	print(nodes_array[0])
+	pass
+
+func _physics_process(_delta: float) -> void:
+	
+	#get input
+	var start_recording_input := Input.is_action_just_pressed(&"start_recording")
+	var stop_recording_input := Input.is_action_just_pressed(&"stop_recording")
+	
+	
+	var start_replay_input := Input.is_action_just_pressed(&"start_replay")
+	var stop_replay_input := Input.is_action_just_pressed(&"stop_replay")
+	
+	if start_recording_input and not is_recording:
+		print("Start Recording")
+		start_recording()
+		is_recording = true
+		
+	if stop_recording_input and is_recording:
+		print("Stop Recording")
+		stop_recording()
+		is_recording = false
+		
+	if start_replay_input and not is_recording:
+		print("Start replaying")
+		start_replay()
+		
+	if stop_replay_input and not is_recording:
+		print("Stop replaying manually")
+		stop_replay()
+		
+	
+	#check_input()
+	
+	update()
