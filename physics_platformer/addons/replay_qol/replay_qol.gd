658db053e8c5c823402e738c2da6ea1810fddb64
Yungsaas <45827473+Yungsaas@users.noreply.github.com>
2025-10-29 11:54:47 +0100

    removed unnecessary gd files
    Removed record panel as it was not being used

--- physics_platformer/addons/replay_qol/replay_qol.gd
+++ physics_platformer/addons/replay_qol/replay_qol.gd
@@ -1,9 +1,6 @@
 @tool
 extends EditorPlugin
 
-var record_panel
-const RECORD_PANEL = preload("res://addons/replay_qol/Record_Panel.tscn")
-
 var selection_panel
 const SELECTION_PANEL = preload("res://addons/replay_qol/Selection_Panel.tscn")
 
@@ -14,8 +11,6 @@ var data_panels:Data_Panels = Data_Panels.new()
 
 
 func _enter_tree() -> void:
-	record_panel = RECORD_PANEL.instantiate()
-	add_control_to_bottom_panel(record_panel, "Record_Panel")
 	
 	selection_panel = SELECTION_PANEL.instantiate()
 	selection_panel.editor_interface = get_editor_interface() # Pass editor interface to panel
@@ -27,12 +22,7 @@ func _enter_tree() -> void:
 	data_panel.data_panels = data_panels
 	data_panel.data_panels.add_position_screen(data_panel.position_text)
 	data_panel.data_panels.add_input_screen(data_panel.input_text)
-	
-	
-#	var edited_scene = get_editor_interface().get_edited_scene_root() #try to use this to instantiate a recorder instead of making one manualy
-#	if edited_scene:
-#		var recorder_new := edited_scene.get_node("Recorder") as Recorder
-#		recorder_new.set_input_screen(data_panel.input_text)
+
 	# Add recording key mappings
 	var startReplayKey := "input/start_replay"
 	if not ProjectSettings.has_setting(startReplayKey):
@@ -56,8 +46,6 @@ func _enter_tree() -> void:
 		ProjectSettings.add_property_info({"name": groupKey, "type":TYPE_BOOL, "usage": PROPERTY_USAGE_GROUP})
 
 func _exit_tree() -> void:
-	remove_control_from_bottom_panel(record_panel)
-	record_panel.queue_free()
 	
 	remove_control_from_docks(selection_panel)
 	selection_panel.queue_free()
--- test_project/main.gd
+++ test_project/main.gd
@@ -2,11 +2,4 @@ extends Node2D
 
 
 func _ready() -> void:
-	_custom()
-	
-	
-func _custom() -> void:
-	var my_class:Temp_save_replay = Temp_save_replay.new()
-	my_class.add_node(%TestNode)
-	my_class.add_node($AnimatedSprite2D)
-	my_class.debug_print_array()
+	pass
